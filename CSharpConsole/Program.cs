ee767d6a5bc93328e851f8cd81becba69ae9d091
Bao Pham Desktop <bdpham00@gmail.com>
2021-08-18 01:03:23 -0700

    Linked List

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -6,7 +6,10 @@ namespace CSharpConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedListSolution solution = new LinkedListSolution();
+            var ans = solution.AddTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))));
+            Console.WriteLine(ans);
+            Console.Read();
         }
     }
 }
