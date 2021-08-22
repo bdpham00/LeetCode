9dfc0c737bd5c1a9f997537221b0365e0b8c7105
Bao D Pham <bdpham00@gmail.com>
2021-08-21 22:50:39 -0700

    Longest Substring

---
+++ CSharpConsole/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().LengthOfLongestSubstring("abcabcbb")) ;
+            Console.Read();
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            string repeatCharacter = "";
+            List<string> subStrings = new List<string>(); 
+            if (!string.IsNullOrEmpty(s))
+            {
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    repeatCharacter += s[i];
+                    if (repeatCharacter[i] == s[i + 1])
+                    {
+                        subStrings.Add(repeatCharacter); 
+                        repeatCharacter = "";
+                    }
+                }
+            }
+
+            return subStrings.OrderByDescending(x => x.Length).First().Length;
+        }
+    }
+}
